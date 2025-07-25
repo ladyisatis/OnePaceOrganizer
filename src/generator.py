d1f6dbef7776cd40dc1373eb03a85badfaf0fc97
Isatis <isatis@foxes.gay>
2025-07-24 17:35:01 -0700

    Update metadata and do an auto-metadata updater

--- src/generator.py
+++ src/generator.py
@@ -1,114 +1,209 @@
 import traceback
 import re
 import sys
-import click
 import orjson
+import httpx
+import datetime
+import io
+import os
 from csv import DictReader as CSVReader
 from yaml import dump as YamlDump, safe_load as YamlLoad
 from pathlib import Path
+from httpx_retries import RetryTransport, Retry
 
-@click.group()
-def generate():
-    pass
+def update():
+    GCLOUD_API_KEY=os.environ['GCLOUD_API_KEY'] if 'GCLOUD_API_KEY' in os.environ else ''
+    if GCLOUD_API_KEY == "":
+        print("Skipping: GCLOUD_API_KEY is empty")
+        return
 
-@click.command()
-@click.option('--file', default='One Pace Episode Descriptions - Arcs.csv')
-@click.option('--outfile', default='./data/seasons.yml')
-def arcs(file, outfile):
+    ONE_PACE_EPISODE_GUIDE_ID="1HQRMJgu_zArp-sLnvFMDzOyjdsht87eFLECxMK858lA"
+    ONE_PACE_EPISODE_DESC_ID="1M0Aa2p5x7NioaH9-u8FyHq6rH3t5s6Sccs8GoC6pHAM"
+
+    PATTERN_END_NUMBER = r'(\d+)'
+    PATTERN_CHAPTER_EPISODE = r'\b\d+(?:-\d+)?(?:,\s*\d+(?:-\d+)?)*\b'
+
+    out_seasons = {}
+    out_episodes = {}
+    season_eps = {}
+
     try:
-        out = {}
+        retry = Retry(total=999, backoff_factor=0.5)
+        spreadsheet = {"sheets": []}
 
-        with Path(file).open(mode='r', newline='', encoding='utf-8') as in_f:
-            reader = CSVReader(in_f)
-            for row in reader:
-                if row['part'] == '':
+        with httpx.Client(transport=RetryTransport(retry=retry)) as client:
+            with client.stream("GET", f"https://docs.google.com/spreadsheets/d/{ONE_PACE_EPISODE_DESC_ID}/export?gid=2010244982&format=csv", follow_redirects=True) as resp:
+                reader = CSVReader(resp.iter_lines())
+
+                for row in reader:
+                    if 'title_en' not in row or row['title_en'] == '' or row['part'] == '':
+                        continue
+
+                    part = int(row['part'])
+
+                    if part == 11:
+                        part = 10
+                    elif part == 10:
+                        part = 11
+                    elif part == 99:
+                        part = 0
+
+                    out_seasons[part] = {
+                        "saga": row['saga_title'],
+                        "title": row['title_en'],
+                        "description": row['description_en']
+                    }
+
+            r = client.get(f"https://sheets.googleapis.com/v4/spreadsheets/{ONE_PACE_EPISODE_GUIDE_ID}?key={GCLOUD_API_KEY}")
+            spreadsheet = orjson.loads(r.content)
+
+            for season, sheet in enumerate(spreadsheet['sheets']):
+                sheetId = sheet['properties']['sheetId']
+                if sheetId == 0:
                     continue
 
-                part = int(row['part'])
-                out[part] = {
-                    'saga': row['saga_title'],
-                    'title': row['title_en'],
-                    'description': row['description_en']
-                }
+                season_title = sheet['properties']['title']
 
-                click.echo(f"adding s{part}: {out[part]}")
+                if season_title != out_seasons[season]['title']:
+                    out_seasons[season]['title'] = season_title
 
-        with Path(outfile).open(mode='w', encoding='utf-8') as f:
-            YamlDump(data=out, stream=f, sort_keys=False, allow_unicode=True)
+                with client.stream("GET", f"https://docs.google.com/spreadsheets/d/{ONE_PACE_EPISODE_GUIDE_ID}/export?gid={sheetId}&format=csv", follow_redirects=True) as resp:
+                    reader = CSVReader(resp.iter_lines())
 
-        click.echo(f"\nwrote to file {str(Path(outfile).resolve())}")
-    except Exception as e:
-        click.echo(traceback.format_exc())
+                    for row in reader:
+                        if 'MKV CRC32' not in row:
+                            continue
+
+                        id = row['One Pace Episode'] if 'One Pace Episode' in row else ''
+                        if id == '' and ' One Pace Episode' in row:
+                            id = row[' One Pace Episode']
+
+                        chapters = row['Chapters'] if 'Chapters' in row else ''
+                        anime_episodes = row['Episodes'] if 'Episodes' in row else ''
+                        release_date = row['Release Date'] if 'Release Date' in row else ''
+                        mkv_crc32 = row['MKV CRC32'] if 'MKV CRC32' in row else ''
+                        mkv_crc32_ext = row['MKV CRC32 (Extended)'] if 'MKV CRC32 (Extended)' in row else ''
+
+                        if mkv_crc32 == '':
+                            continue
+
+                        if id == '' or chapters == '' or anime_episodes == '' or release_date == '' or mkv_crc32 == '':
+                            print(f"Skipping: {row} (no data)")
+                            continue
+
+                        match = re.search(PATTERN_END_NUMBER, id)
+                        if match:
+                            episode = int(match.group(1))
+                        else:
+                            episode = 1
+
+                        match = re.search(PATTERN_CHAPTER_EPISODE, chapters)
+                        if match:
+                            chapters = match.group(0).replace(" ", "").replace(",", ", ")
+
+                        match = re.search(PATTERN_CHAPTER_EPISODE, anime_episodes)
+                        if match:
+                            anime_episodes = match.group(0).replace(", ", ",").replace(",", ", ")
+
+                        release_date_group = release_date.split(".")
+                        if len(release_date_group) == 3:
+                            release_date = datetime.date(int(release_date_group[0]), int(release_date_group[1]), int(release_date_group[2]))
+                        else:
+                            release_date_group = release_date.split("-")
+                            if len(release_date_group) == 3:
+                                release_date = datetime.date(int(release_date_group[0]), int(release_date_group[1]), int(release_date_group[2]))
+                            else:
+                                print(f"Skipping: {row} (invalid release date)")
+                                continue
+
+                        out_episodes[mkv_crc32] = {
+                            "season": season,
+                            "episode": episode,
+                            "title": "",
+                            "description": "",
+                            "manga_chapters": chapters,
+                            "anime_episodes": anime_episodes,
+                            "released": release_date.isoformat()
+                        }
+
+                        if len(mkv_crc32_ext) > 0:
+                            out_episodes[mkv_crc32_ext] = out_episodes[mkv_crc32]
+
+                        season_eps[f"{season_title} {episode}"] = [mkv_crc32] if mkv_crc32_ext == '' else [mkv_crc32, mkv_crc32_ext]
+
+                with client.stream("GET", f"https://docs.google.com/spreadsheets/d/{ONE_PACE_EPISODE_DESC_ID}/export?gid=0&format=csv", follow_redirects=True) as resp:
+                    reader = CSVReader(resp.iter_lines())
+
+                    for row in reader:
+                        if 'arc_title' not in row:
+                            continue
+
+                        season = row['arc_title']
+                        episode = row['arc_part']
+                        title = row['title_en']
+                        description = row['description_en']
+
+                        if season == '' or episode == '' or title == '':
+                            continue
+
+                        key = f"{season} {episode}"
+                        if key not in season_eps:
+                            continue
+
+                        for crc32 in season_eps[key]:
+                            out_episodes[crc32]["title"] = title
+                            out_episodes[crc32]["description"] = description
+
+        for crc32, data in out_episodes.items():
+            file_path = Path(".", "data", "episodes", f"{crc32}.yml")
+
+            season = data['season']
+            episode = data['episode']
+
+            if season == 99:
+                season = 0
+            elif season > 90:
+                episode = season
+                season = 0
+
+            out = (
+                f"season: {season}\n"
+                f"episode: {episode}\n"
+                "\n"
+                "{title}"
+                "# originaltitle: \n"
+                "# sorttitle: \n"
+                "{description}"
+                f"manga_chapters: {data['manga_chapters']}\n"
+                f"anime_episodes: {data['anime_episodes']}\n"
+                "\n"
+                "# rating: TV-14\n"
+                f"released: {data['released']}\n"
+                "\n"
+                "hashes:\n"
+                f"  crc32: {crc32}\n"
+                "# blake2: \n"
+            )
+
+            # attempt to bypass some unicode nonsense
+            out = out.replace("{title}", YamlDump({"title": data['title']}, allow_unicode=True), 1)
+            out = out.replace("{description}", YamlDump({"description": data['description']}, allow_unicode=True), 1)
+
+            with file_path.open(mode='w') as f:
+                f.write(out)
+
+        season_path = Path(".", "data", "seasons.yml")
+        with season_path.open(mode='w') as f:
+            YamlDump(data=out_seasons, stream=f, allow_unicode=True)
+
+    except:
+        print(traceback.format_exc())
         sys.exit(1)
 
-generate.add_command(arcs)
-
-@click.command()
-@click.option('--file', default='One Pace Episode Descriptions - Episodes.csv')
-@click.option('--seasonsyml', default='./data/seasons.yml')
-@click.option('--outdir', default='./data/episodes')
-def episodes(file, seasonsyml, outdir):
-    try:
-        seasons = {}
-        with Path(seasonsyml).open(mode='r', encoding='utf-8') as f:
-            seasons = YamlLoad(stream=f)
-
-        title_to_season = {
-            season_data['title']: season_num
-            for season_num, season_data in seasons.items()
-        }
-
-        with Path(file).open(mode='r', newline='', encoding='utf-8') as f:
-            reader = CSVReader(f)
-            for row in reader:
-                arc_title = row['arc_title']
-                if arc_title == '':
-                    continue
-
-                season_num = title_to_season.get(arc_title)
-                if season_num is None:
-                    season_num = 99
-
-                out = {
-                    "season": season_num,
-                    "episode": int(row['arc_part']),
-                    "title": row['title_en'],
-                    "description": row['description_en'],
-                    "manga_chapters": "",
-                    "anime_episodes": "",
-                    "released": "2000-01-01 00:00"
-                }
-
-                if 'filename' not in row or row['filename'] == "":
-                    out_filename = f"{arc_title}_{out['episode']}.yml"
-                else:
-                    match = re.search(r'\[([A-F0-9]{8})\]\.(?:mkv|mp4)$', row['filename'], re.IGNORECASE)
-                    if match:
-                        out_filename = f"{match.group(1)}.yml"
-                    else:
-                        out_filename = f"{arc_title}_{out['episode']}.yml"
-
-                total_file = Path(outdir, out_filename)
-                if total_file.exists():
-                    total_file = Path(outdir, out_filename.replace('.yml', '_2.yml'))
-                
-                with total_file.open(mode='w', encoding='utf-8') as o_f:
-                    click.echo(f"writing to {total_file.name}: {out}")
-                    YamlDump(data=out, stream=o_f, allow_unicode=True, sort_keys=False)
-
-    except Exception as e:
-        click.echo(traceback.format_exc())
-        sys.exit(1)
-
-generate.add_command(episodes)
-
-@click.command()
-@click.option('--datadir', default='./data')
-@click.option('--json', default='./data.json')
-def json(datadir, json):
-    tvshow_yml = Path(datadir, "tvshow.yml")
-    seasons_yml = Path(datadir, "seasons.yml")
-    episodes_dir = Path(datadir, "episodes")
+def generate_json():
+    tvshow_yml = Path(".", "data", "tvshow.yml")
+    seasons_yml = Path(".", "data", "seasons.yml")
+    episodes_dir = Path(".", "data", "episodes")
 
     out = {}
 
@@ -127,9 +222,15 @@ def json(datadir, json):
     
     out["episodes"] = episodes
 
-    with Path(json).open(mode='wb') as f:
+    with Path(".", "data.json").open(mode='wb') as f:
         f.write(orjson.dumps(out, option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SORT_KEYS ))
 
-generate.add_command(json)
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == 'update':
+        update()
+        return
 
-generate()
+    generate_json()
+
+if __name__ == '__main__':
+    main()
