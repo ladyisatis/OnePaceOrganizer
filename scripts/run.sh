a2222d9ea10358cb6b0e9b80eb8c27c2162ccd7d
Isatis <isatis@foxes.gay>
2025-08-01 15:06:54 -0700

    Python version checks

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,6 +8,14 @@ if ! command -v python3 &> /dev/null; then
     exit 1
 fi
 
+PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
+REQUIRED_VERSION="3.13"
+
+if [ ! "$(printf '%s\n' "$REQUIRED_VERSION" "$PYTHON_VERSION" | sort -V | head -n1)" = "$REQUIRED_VERSION" ]; then
+    echo "This program requires Python 3.13 or newer."
+    exit 1
+fi
+
 if [ ! -d "venv" ]; then
     python3 -m venv venv
     . venv/bin/activate
